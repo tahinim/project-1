6e77c49a4730fa9032fd99101a8254bdece2024e
tahinim <tahinim@hotmail.com>
2015-03-13 22:28:14 +0530

    boxes

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ var $box = $('.box');
 $btnShow.on ('click', function() {
     $box.toggleClass('js-box-show-hide');
 });
+
+var $btnExpand = $('.btn-collapse-expand');
+var $panel = $('.panel');
+
+$btnExpand.on ('click', function () {
+    $panel.toggleClass('js-panel-collapse-expand');
+});
